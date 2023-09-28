d0fab2a464b51968ae1912323a2d2ab8ec3879f9
UdotBdot <Frederic.sin@proton.me>
2023-09-28 16:16:09 +0200

    ADD: Dynamic Bullets according to Slides length

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,74 @@
+// Tableau de diapositives
 const slides = [
-	{ // [0]
-		"image":"./assets/images/slideshow/slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	{
+	  image: "./assets/images/slideshow/slide1.jpg",
+	  tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
 	},
-	{ // [1]
-		"image":"./assets/images/slideshow/slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	{
+	  image: "./assets/images/slideshow/slide2.jpg",
+	  tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
 	},
-	{ // [2]
-		"image":"./assets/images/slideshow/slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	{
+	  image: "./assets/images/slideshow/slide3.jpg",
+	  tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
 	},
-	{ // [3]
-		"image":"./assets/images/slideshow/slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-];
-const leftEl = document.querySelector(".arrow_left")
-const rightEl = document.querySelector(".arrow_right")
-const dots = document.querySelector(".dots")
-const dotsEl = dots.querySelectorAll(".dot");
-const img = document.querySelector(".banner-img")
-const txt = document.querySelector("p")
-let i = 0 
-
-leftEl.addEventListener("click", function(){
+	{
+	  image: "./assets/images/slideshow/slide4.png",
+	  tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+	},
+  ];
+  
+  // Variables
+  let i = 0;
+  const leftEl = document.querySelector(".arrow_left");
+  const rightEl = document.querySelector(".arrow_right");
+  const dotsContainer = document.querySelector(".dots");
+  const img = document.querySelector(".banner-img");
+  const txt = document.querySelector("p");
+  
+  // Créez et ajoutez les éléments .dot enfants en fonction de la longueur du tableau slides
+  for (let j = 0; j < slides.length; j++) {
+	const dot = document.createElement("div");
+	dot.classList.add("dot");
+	dotsContainer.appendChild(dot);
+  }
+  
+  // Sélectionnez à nouveau les éléments .dot après les avoir ajoutés
+  const dotsEl = dotsContainer.querySelectorAll(".dot");
+  
+  // Ajoutez la classe dot_selected à la première bullet (index 0)
+  dotsEl[0].classList.add("dot_selected");
+  
+  // Écouteurs d'événements pour les boutons gauche et droite
+  leftEl.addEventListener("click", function () {
 	i--;
 	if (i === -1) {
-		i = slides.length - 1;
+	  i = slides.length - 1;
 	}
 	img.src = slides[i].image;
-	txt.innerHTML = slides[i].tagLine
+	txt.innerHTML = slides[i].tagLine;
 	dynamicBullets(i);
-})
-
-rightEl.addEventListener("click", function() {
+  });
+  
+  rightEl.addEventListener("click", function () {
 	i++;
 	if (i === slides.length) {
-		i = 0
+	  i = 0;
 	}
 	img.src = slides[i].image;
-	txt.innerHTML = slides[i].tagLine
+	txt.innerHTML = slides[i].tagLine;
 	dynamicBullets(i);
-})	
-
-function dynamicBullets(count) {
-		dotsEl.forEach((dot, i) => {
-		  if (i === count) {
-			dot.classList.add("dot_selected");
-		  } else {
-			dot.classList.remove("dot_selected");
-		  }
-		});
-	}
+  });
   
+  // Fonction pour mettre à jour les bullets dynamiquement
+  function dynamicBullets(count) {
+	dotsEl.forEach((dot, index) => {
+	  if (index === count) {
+		dot.classList.add("dot_selected");
+	  } else {
+		dot.classList.remove("dot_selected");
+	  }
+	});
+  }
 
   
